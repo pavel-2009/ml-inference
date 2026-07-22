e4af088c5568f3bb1a2b8a6fbc43101319f7318d
pavel-2009 <ms.radionova@bk.ru>
2026-07-22 11:45:49 +0300

    created factory

--- include/models/model_factory.hpp
+++ include/models/model_factory.hpp
@@ -3,6 +3,8 @@
 
 #include <nlohmann/json.hpp>
 #include <fstream>
+#include <memory>
+#include <stdexcept>
 
 using nlohmann::json;
 
@@ -14,25 +16,48 @@ enum class ModelType
 };
 
 class ModelFactory {
-    public:
-        static BaseModel* createModel(ModelType type) {
-            switch (type) {
-                case(ModelType::AVERAGE) {
-                    std::ifstream file("models/average.json")
-                }
-                case(ModelType::SORT) {
-                    std::ifstream file("models/sort.json")
-                }
-                case(ModelType::SUM) {
-                    std::ifstream file("models/sum.json")
-                }
-                default {
-                    std::ifstream file("models/average.json")
-                }
-            }
-
-            json model_config;
-            file >> model_config;
+public:
+    static std::unique_ptr<BaseModel> createModel(ModelType type) {
+        std::string file_path;
+        
+        switch (type) {
+            case ModelType::AVERAGE:
+                file_path = "models/average.json";
+                break;
+            case ModelType::SORT:
+                file_path = "models/sort.json";
+                break;
+            case ModelType::SUM:
+                file_path = "models/sum.json";
+                break;
+            default:
+                file_path = "models/average.json";
+                break;
         }
-
-}
+        
+        return createModelFromFile(file_path);
+    }
+    
+    static std::unique_ptr<BaseModel> createModelFromFile(const std::string& file_path) {
+        std::ifstream file(file_path);
+        
+        if (!file.is_open()) {
+            throw std::runtime_error("Failed to open model config file: " + file_path);
+        }
+        
+        json model_config;
+        file >> model_config;
+        
+        // Парсим JSON в ModelInfo
+        ModelInfo info;
+        info.id = model_config.value("id", "");
+        info.type = model_config.value("type", "");
+        info.name = model_config.value("name", "");
+        info.version = model_config.value("version", "");
+        info.author = model_config.value("author", "");
+        info.description = model_config.value("description", "");
+        info.enabled = model_config.value("enabled", true);
+        
+        return std::make_unique<BaseModel>(std::move(info));
+    }
+};

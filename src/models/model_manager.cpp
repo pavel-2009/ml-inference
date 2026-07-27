630f46c9aad56b6cda3b0011e1960ba510d7c1a3
pavel-2009 <ms.radionova@bk.ru>
2026-07-27 21:25:39 +0300

    updated manager

--- src/models/model_manager.cpp
+++ src/models/model_manager.cpp
@@ -6,7 +6,7 @@
 #include <mutex>
 
 
-std::shared_ptr<BaseModel> ModelManager::get(const std::string& id) {
+std::shared_ptr<IModel> ModelManager::get(const std::string& id) {
 
     std::shared_lock lock(mutex_);
     
@@ -20,7 +20,7 @@ std::shared_ptr<BaseModel> ModelManager::get(const std::string& id) {
     
 }
 
-void ModelManager::add(const std::string& id, std::shared_ptr<BaseModel> model) {
+void ModelManager::add(const std::string& id, std::shared_ptr<IModel> model) {
     std::unique_lock lock(mutex_);
 
     models_.emplace(id, std::move(model));

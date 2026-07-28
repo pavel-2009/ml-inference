27232166294bb282f5926937eacdd5a39231f8f0
pavel-2009 <ms.radionova@bk.ru>
2026-07-28 21:41:41 +0300

    moved to shared

--- include/models/model_factory.hpp
+++ include/models/model_factory.hpp
@@ -15,5 +15,5 @@ enum class ModelType
 
 class ModelFactory {
 public:
-    static std::unique_ptr<IModel> createModel(ModelType type, ModelInfo info);
+    static std::shared_ptr<IModel> createModel(ModelType type, ModelInfo info);
 };
--- src/models/model_factory.cpp
+++ src/models/model_factory.cpp
@@ -9,16 +9,16 @@
 
 using nlohmann::json;
 
-std::unique_ptr<IModel> ModelFactory::createModel(ModelType type, ModelInfo info) {
+std::shared_ptr<IModel> ModelFactory::createModel(ModelType type, ModelInfo info) {
     std::string file_path;
     
     switch (type) {
         case ModelType::AVERAGE:
-            return std::make_unique<AverageModel>(std::move(info));
+            return std::make_shared<AverageModel>(std::move(info));
         case ModelType::SORT:
-            return std::make_unique<SortModel>(std::move(info));
+            return std::make_shared<SortModel>(std::move(info));
         case ModelType::SUM:
-            return std::make_unique<SumModel>(std::move(info));
+            return std::make_shared<SumModel>(std::move(info));
         default:
             throw std::runtime_error("Unknown model type: " + info.type);
     }

8fcee4d09dea1886bc7d91525d419f89091c6ef5
pavel-2009 <ms.radionova@bk.ru>
2026-07-28 11:59:25 +0300

    created task for threadpool

---
+++ src/async-loader/model-task.cpp
@@ -0,0 +1,25 @@
+#include "base_task.hpp"
+#include "async-loader.hpp"
+
+#include <filesystem>
+#include <iostream>
+
+
+class ModelLoadTask : public BaseTask {
+    public:
+        ModelLoadTask(AsyncLoader* loader, std::filesystem::path file)
+            : loader_(loader), file_(std::move(file)) {}
+        
+        void execute() override {
+            try {
+                loader_->loadModel(file_);
+            } catch (const std::exception& e) {
+                std::cerr << "❌ Ошибка загрузки " << file_.filename() 
+                        << ": " << e.what() << '\n';
+            }
+        }
+        
+    private:
+        AsyncLoader* loader_;
+        std::filesystem::path file_;
+};

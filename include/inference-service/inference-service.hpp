cb19fac83e85a09a69acb4c56275a516b6283a46
pavel-2009 <ms.radionova@bk.ru>
2026-07-30 20:42:27 +0300

    created basic inference template

---
+++ include/inference-service/inference-service.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "model/model_manager.hpp"
+#include "model/inference_response.hpp"
+#include "model/inference_request.hpp"
+
+
+class InferenceService {
+    private:
+        ModelManager& manager_;
+
+    public:
+        explicit InferenceService(ModelManager& manager);
+
+        InferenceResponse infer(const InferenceRequest& request);
+};

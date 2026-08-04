7503f41e67fcab32b1144374fd148b994872f060
pavel-2009 <ms.radionova@bk.ru>
2026-08-04 20:46:20 +0300

    added infer route

--- src/router/router.cpp
+++ src/router/router.cpp
@@ -3,6 +3,8 @@
 #include "http_response.hpp"
 #include "inference_service.hpp"
 
+#include <iostream>
+
 
 explicit Router::Router(InferenceService& inference)
     : inference_(inference) {};
@@ -23,4 +25,41 @@ HttpResponse Router::route(const HttpRequest& request) {
     default:
         return this->models();
     }
+}
+
+
+HttpResponse Router::infer(const HttpRequest& request) {
+
+    HttpResponse response;
+
+    std::optional<InferenceRequest> inference_request = request.inference_request;
+
+    if (!inference_request) {
+        response.status = 400;
+        response.message = "Invalid input";
+        return response;
+    };
+
+    try
+    {
+        InferenceResponse inference_response = inference_.infer(inference_request);
+
+        response.status = 200;
+        response.message = "Success";
+        response.inference_response = inference_response;
+
+        return response;
+    }
+
+    catch(const std::exception& e)
+    {
+        std::string error_msg = e.what();
+        std::cerr << error_msg << '\n';
+
+        response.status = 404;
+        response.message = std::format("Error while processing: {}", error_msg);
+        return response;
+    }
+    
+
 }
